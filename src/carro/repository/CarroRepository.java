4d776dc23a96bc1f17d973bb03436dc3a5b2127e
giova <giovanaximenes14@gmail.com>
2024-07-12 12:59:15 -0300

    PG JAVA 16 Módulo 20

--- src/carro/repository/CarroRepository.java
+++ src/carro/repository/CarroRepository.java
@@ -1,13 +1,13 @@
 package carro.repository;
 
-import carro.model.carroZero;
+import carro.model.CarroZero;
 
 public interface CarroRepository {
 
     // CRUD do Estacionamento
     public void procurarPorId(int idCarro);
     public void listarTodos();
-    public void cadastrar(carroZero carro);
+    public void cadastrar(CarroZero carro);
     public void atualizar(int idCarro, float precoCarroAtualizado);
     public void deletar(int idCarro);
 
